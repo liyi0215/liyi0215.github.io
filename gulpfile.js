191f4a8ea7c69864eda1364dc954bfe1c3519a5d
liyi0215 <ileeyi@163.com>
2016-11-14 11:00:09 +0800

    modify gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,37 @@
 var gulp = require('gulp'),
     plugins = require('gulp-load-plugins')();
-var toPdf = require('htmltopdf');
 
 var asset = {
     js: './public/**/*.js',
     css: './public/**/*.css',
-    html: './public/**/*.html'
+    html: './public/**/*.html',
+    public: './public'
 };
 
-gulp.task('default', ['min-css', 'min-html']);  //    , 'min-js'
+gulp.task('default', ['minCss', 'minHtml']);  //    , 'js'
 
-gulp.task('min-css', function () {
+gulp.task('minCss', function () {
     return gulp.src(asset.css)
         .pipe(plugins.minifyCss())
-        .pipe(gulp.dest('./public'));
+        .pipe(gulp.dest(asset.public));
 });
 
-gulp.task('min-js', function () {
+gulp.task('js', function () {
     return gulp.src(asset.js)
         .pipe(plugins.uglify())
-        .pipe(gulp.dest('./public'));
+        .pipe(gulp.dest(asset.public));
 });
 
-gulp.task('min-html', function () {
-    return gulp.src(asset.html)
-        .pipe(plugins.clean())
-        .pipe(plugins.htmlmin({removeComments: true, minifyJS: true, minifyCSS: true, minifyURLs: true}))
-        .pipe(gulp.dest('./public'));
-});
-
-gulp.task('html2pdf', function () {
-    toPdf.createFromHtml('./public/about/index.html', './public/cv.pdf', function () {
-        console.info('createFromHtml');
-    })
+gulp.task('minHtml', function () {
+    var options = {
+        removeComments: true,//清除HTML注释
+        collapseWhitespace: true,//压缩HTML
+        collapseBooleanAttributes: false,//省略布尔属性的值 <input checked="true"/> ==> <input />
+        removeEmptyAttributes: true,//删除所有空格作属性值 <input id="" /> ==> <input />
+        removeScriptTypeAttributes: false,//删除<script>的type="text/javascript"
+        removeStyleLinkTypeAttributes: false,//删除<style>和<link>的type="text/css"
+        minifyJS: true,//压缩页面JS
+        minifyCSS: true//压缩页面CSS
+    };
+    gulp.src(asset.html).pipe(plugins.htmlmin(options)).pipe(gulp.dest(asset.public));
 });
